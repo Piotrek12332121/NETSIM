aab3d999716548af3203b4b07216a0dcefb0cdb9
BartekR25 <81678674+BartekR25@users.noreply.github.com>
2021-12-21 23:35:47 +0100

    Add files via upload

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -2,3 +2,19 @@
 #define LAB7_STORAGE_TYPES_HPP
 
 #endif //LAB7_STORAGE_TYPES_HPP
+
+class IPackageStockpile{
+public:
+    using const_iterator = std::list<Package>::const_iterator;
+
+    virtual void push(Package&& aPackage) = 0;
+    virtual bool empty() const = 0;
+    virtual size_type size() const = 0;
+
+    virtual std::list<Package>::iterator begin() = 0;
+    virtual const_iterator cbegin() const = 0;
+    virtual std::list<Package>::iterator end() = 0;
+    virtual const_iterator cend() const = 0;
+
+    virtual ~IPackageStockpile() {};
+};
