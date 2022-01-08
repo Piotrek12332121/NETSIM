d66ca3cef864bb1b8c19058f789b1d6efeb2da7d
jakub-sacha <81679552+jakub-sacha@users.noreply.github.com>
2022-01-08 17:54:16 +0100

    Dodana klasa PackageQueue

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,6 +1,11 @@
 #ifndef LAB7_STORAGE_TYPES_HPP
 #define LAB7_STORAGE_TYPES_HPP
 
+#include "types.hpp"
+#include <list>
+#include "package.hpp"
+
+
 class IPackageStockpile{
 public:
     using const_iterator = std::list<Package>::const_iterator;
@@ -15,6 +20,25 @@ public:
     virtual const_iterator cend() const = 0;
 
     virtual ~IPackageStockpile() {};
+};
+
+class PackageQueue : public IPackageQueue {
+public:
+    PackageQueue(PackageQueueType queue_type) : queue_type_(queue_type) {}
+    void push(Package&& other) override;
+    bool empty() override;
+    std::size_t size() override;
+    Package pop() override;
+    PackageQueueType get_queue_type() override;
+
+    const_iterator begin() const override {return queue_.begin();}
+    const_iterator cbegin() const override {return queue_.cbegin();}
+    const_iterator end() const override {return queue_.end();}
+    const_iterator cend() const override {return queue_.cend();}
+
+private:
+    std::list<Package> queue_;
+    PackageQueueType queue_type_;
 };
 
 #endif //LAB7_STORAGE_TYPES_HPP
--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,1 +1,34 @@
+#include "storage_types.hpp"
 
+void PackageQueue::push(Package&& other){
+    queue_.emplace_back(std::move(other));
+}
+
+bool PackageQueue::empty() {
+    return queue_.empty();
+}
+
+std::size_t PackageQueue::size(){
+    return queue_.size();
+}
+
+Package PackageQueue::pop() {
+    switch(queue_type_) {
+        case PackageQueueType::LIFO: {
+            Package popped_element_back = std::move(queue_.back());
+            queue_.pop_back();
+            return popped_element_back;
+        }
+        case PackageQueueType::FIFO: {
+            Package popped_element_front = std::move(queue_.front());
+            queue_.pop_front();
+            return popped_element_front;
+        }
+        default: throw;
+    }
+
+}
+
+PackageQueueType PackageQueue::get_queue_type() {
+    return queue_type_;
+}
