436ce5e6d887d2ff6acfbee76c45766236971f44
Dominik-Salabura <79972094+Dominik-Salabura@users.noreply.github.com>
2022-01-09 13:25:05 +0100

    Szkielet pod etap II projektu i przydział zadań
    Dotychczasowy szkielet folderu został zaaktualizowany o nowe pliki: "nodes", "helpers" i "factory".
    Drobna zmiana w klasie IPackageStockpile w pliku "storage_types.hpp".
    Do repozytorium dodano folder z przydziałem zadań, gdzie znajduje się zaległy przydział zadań w ramach I etapu. Przydział zadań do wykonania w ramach II etapu został zamieszczony w folderze z przydziałem zadań.

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -12,7 +12,7 @@ public:
 
     virtual void push(Package&& aPackage) = 0;
     virtual bool empty() const = 0;
-    virtual size_type size() const = 0;
+    virtual size_t size() const = 0;
 
     virtual std::list<Package>::iterator begin() = 0;
     virtual const_iterator cbegin() const = 0;
@@ -25,11 +25,11 @@ public:
 class PackageQueue : public IPackageQueue {
 public:
     PackageQueue(PackageQueueType queue_type) : queue_type_(queue_type) {}
-    void push(Package&& other) override;
-    bool empty() override;
-    std::size_t size() override;
+    void push(Package&& other) override {queue_.emplace_back(std::move(other));}
+    bool empty() override {return queue_.empty();}
+    std::size_t size() override {return queue_.size();}
     Package pop() override;
-    PackageQueueType get_queue_type() override;
+    PackageQueueType get_queue_type() override {return queue_type_;}
 
     const_iterator begin() const override {return queue_.begin();}
     const_iterator cbegin() const override {return queue_.cbegin();}
