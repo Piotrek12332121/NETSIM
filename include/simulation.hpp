ce02b24f87e86b4b8df1d9d0c86607229bc05a15
jakub-sacha <81679552+jakub-sacha@users.noreply.github.com>
2022-01-17 11:05:58 +0100

    simulation

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -1,4 +1,10 @@
 #ifndef NETSIM_SIMULATION_HPP
 #define NETSIM_SIMULATION_HPP
 
+#include "reports.hpp"
+#include "factory.hpp"
+#include "types.hpp"
+#include <functional>
+
+void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)> rf);
 #endif //NETSIM_SIMULATION_HPP
---
+++ src/simulation.cpp
@@ -0,0 +1,15 @@
+#include "simulation.hpp"
+
+void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)> rf){
+    if (f.is_consistent()) {
+        for (Time i = 1; i <= d; i++) {
+            f.do_deliveries(i);
+            f.do_package_passing();
+            f.do_work(i);
+            rf(f, i);
+        }
+    }
+    else {
+        throw std::logic_error("Fabryka nie jest konsytentna");
+    }
+}
